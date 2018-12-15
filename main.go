19645c375c23e87d2c959349c0cf87e0b2e744ec
YashishDua <yashish.dua@gmail.com>
2018-12-16 02:58:35 +0530

    refactor

--- main.go
+++ main.go
@@ -4,14 +4,12 @@ import (
   "log"
   "net/http"
   "github.com/go-chi/chi"
-  "github.com/go-chi/chi/middleware"
   "smart-signal/util"
   "smart-signal/endpoints"
 )
 
 func main() {
   router := chi.NewRouter()
-  router.Use(middleware.Logger)
   router.Get("/", util.ResponseWrapper(Hello))
   router.Get("/traffic/data", util.ResponseWrapper(endpoints.GetTrafficData))
   log.Fatal(http.ListenAndServe(":8000", router))
