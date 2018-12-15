c9363a5eecc6d5d2e399b7dd68520ffcbf915590
YashishDua <yashish.dua@gmail.com>
2018-12-16 04:54:12 +0530

    t

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
   "fmt"
   "os"
   "github.com/go-chi/chi"
+  "github.com/go-chi/cors"
   "smart-signal/util"
   "smart-signal/endpoints"
 )
@@ -18,6 +19,17 @@ func main() {
     return
   }
   log.Println(port)
+  cors := cors.New(cors.Options{
+    // AllowedOrigins: []string{"https://foo.com"}, // Use this to allow specific origin hosts
+    AllowedOrigins:   []string{"*"},
+    // AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+    AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+    AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+    ExposedHeaders:   []string{"Link"},
+    AllowCredentials: true,
+    MaxAge:           300, // Maximum value not ignored by any of major browsers
+  })
+  router.Use(cors.Handler)
   router.Get("/", util.ResponseWrapper(Hello))
   router.Post("/traffic/data", util.ResponseWrapper(endpoints.GetTrafficData))
   log.Fatal(http.ListenAndServe(":" + port, router))
