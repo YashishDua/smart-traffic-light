bb804e048ab96af1b410c7535f4f92676753acd3
YashishDua <yashish.dua@gmail.com>
2018-12-16 03:08:59 +0530

    refactor

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
   "log"
   "net/http"
   "os"
+  "fmt"
   "github.com/go-chi/chi"
   "smart-signal/util"
   "smart-signal/endpoints"
@@ -12,10 +13,14 @@ import (
 func main() {
   router := chi.NewRouter()
   port := os.Getenv("PORT")
+  if port == "" {
+    fmt.Errorf("$PORT not set")
+    return
+  }
   log.Println(port)
   router.Get("/", util.ResponseWrapper(Hello))
   router.Get("/traffic/data", util.ResponseWrapper(endpoints.GetTrafficData))
-  log.Fatal(http.ListenAndServe(":8000", router))
+  log.Fatal(http.ListenAndServe(":" + port, router))
 }
 
 //HealhCheck API
